cb8fc3696eefd5607518e386bf6ef46fab2fe1d3
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-03-18 13:39:27 -0400

    Update BubbleSortArray.java

--- src/main/java/org/lrwham/sorting/BubbleSortArray.java
+++ src/main/java/org/lrwham/sorting/BubbleSortArray.java
@@ -2,21 +2,24 @@ package org.lrwham.sorting;
 
 public class BubbleSortArray implements SortArray {
     @Override
-    public <E extends Comparable<E>> E[] sort(E[] unsorted){
-        for(int i = 0; i < unsorted.length; i++){
+    public <E extends Comparable<E>> E[] sort(E[] unsorted) {
+        int n = unsorted.length;
+
+        for (int i = 0; i < n - 1; i++) {
             boolean swapped = false;
-            for(int j = i + 1; j < unsorted.length; j++){
-                if(unsorted[i].compareTo(unsorted[j]) > 0){
-                    E temp = unsorted[i];
-                    unsorted[i] = unsorted[j];
-                    unsorted[j] = temp;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (unsorted[j].compareTo(unsorted[j + 1]) > 0) {
+                    E temp = unsorted[j];
+                    unsorted[j] = unsorted[j + 1];
+                    unsorted[j + 1] = temp;
                     swapped = true;
                 }
             }
-            if(swapped == false) return unsorted;
+            if (swapped == false) {
+                break;
+            }
         }
 
         return unsorted;
     }
-
 }
